7cefa41a12095bbbea696cebae201e3f60436423
DominicDev <s0l0estudi4r@gmail.com>
2026-03-20 17:13:44 +0100

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'REMY ZERO';
+export const nameSongs = [
+	"SAVE ME", /*1*/
+	"THE GOLDEN HUM", /*2*/
+	"GLORIOUS #1", /*3*/
+	"OUT / IN", /*4*/
+	"BITTER", /*5*/
+	"PERFECT MEMORY", /*6*/
+	"BELONG", /*7*/
+	"OVER THE RAILS", /*8*/
+	"SMILE", /*9*/
+	"I'M NOT AFRAID", /*10*/
+	"IMPOSSIBILITY", /*11*/
+	"SUB BALLOON", /*12*/
+	"HERMES BIRD", /*13*/
+	"LIFE IN RAIN", /*14*/
+	"HOLLOW", /*15*/
+	"PROBLEM", /*16*/
+	"WITHER VULCAN", /*17*/
+	"YELLOW LIGHT", /*18*/
+	"MOTORCYCLE", /*19*/
+	"FAIR", /*20*/
+]
